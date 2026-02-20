2c27d1aec86aef7588baf9c860b6bcf78325ee4a
Thirstypooch <juancocchi6@gmail.com>
2026-02-20 16:14:09 -0500

    Remove footer from web UI

--- example/index.php
+++ example/index.php
@@ -89,11 +89,6 @@
 			</div>
 			<div style="display:none" class="alert alert-danger" role="alert" id="error">
 			</div>
-			<footer class="footer text-center">
-				<div class="col">
-					<p><small>&copy; 2015 - 2017 JossMP - Derechos Reservados</small></p>
-				</div>
-			</footer>
 		</div>
 		<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
